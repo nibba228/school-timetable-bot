5580ef38f3b2b3ad03d129ab35d382b734d0ac11
nibba228 <witch21277@gmail.com>
2021-10-28 14:43:15 +0300

    Убрал переменную redis_thread

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from app.handlers.user.choosing_class import register_handlers_choosing_class
 from app.handlers.user.timetable import register_handlers_timetable
 
 from loader import (WEBAPP_HOST, WEBAPP_PORT, WEBHOOK_PATH, WEBHOOK_URL,
-                    dp, bot, logger, redis_thread)
+                    dp, bot, logger)
 
 
 async def set_commands(bot: Bot):
@@ -39,7 +39,6 @@ async def on_startup(dp: Dispatcher):
 async def on_shutdown(dp: Dispatcher):
     await dp.storage.close()
     await dp.storage.wait_closed()
-    redis_thread.join()
     
     logger.warning('Shutting down bot...')
 
